9706774b37eedf82ddc8db8d7885e2228555f846
Max Horstmann <max.horstmann@gmail.com>
2016-01-17 15:43:49 -0500

    hc round 1

---
+++ Facebook-Hacker-Cup/2016/round1/1-competition/1-competition/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _1_competition
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string inputFile = "coding_contest_creation.txt";
+            const string outputFile = "output.txt";
+
+            using (StreamReader streamReader = File.OpenText(inputFile))
+            {
+                using (StreamWriter streamWriter = File.CreateText(outputFile))
+                {
+                    var T = int.Parse(streamReader.ReadLine());
+
+                    for (var t = 1; t <= T; t++)
+                    {
+                        var N = int.Parse(streamReader.ReadLine());
+                        var D = streamReader.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                        var solution = ComputeSolution(D);
+                        var output = string.Format("Case #{0}: {1}", t, solution);
+                        Console.WriteLine(output);
+                        streamWriter.WriteLine(output);
+
+                    }
+                    streamWriter.Close();
+                }
+                streamReader.Close();
+            }
+
+        }
+
+        static int ComputeSolution(int[] D)
+        {
+            if (D.Length == 0) return 0;
+            if (D.Length == 1) return 3;
+            
+            // split if not strictly increasing or gap > 30
+
+            for (var i = 1; i < D.Length; i++)
+            {
+                if ((D[i - 1] >= D[i]) || (D[i] - D[i-1] > 30))
+                {
+                    var left = ComputeSolution(D.Take(i).ToArray());
+                    var right = ComputeSolution(D.Skip(i).ToArray());
+                    return left + right;
+                }
+            }
+
+            
+            var gaps = 0;
+            for (var i = 1; i < D.Length; i++)
+            {
+                gaps += (D[i] - D[i - 1] - 1)/10;
+            }
+
+            var mod = (D.Length + gaps)%4;
+            if (mod == 0) return gaps;
+            return gaps + 4 - mod;
+        }
+
+    }
+}
---
+++ Facebook-Hacker-Cup/2016/round1/2-laundromatt/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _2_laundromatt
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string inputFile = "laundro_matt_example_input.txt";
+            const string outputFile = "output.txt";
+
+            using (StreamReader streamReader = File.OpenText(inputFile))
+            {
+                using (StreamWriter streamWriter = File.CreateText(outputFile))
+                {
+                    var T = int.Parse(streamReader.ReadLine());
+
+                    for (var t = 1; t <= T; t++)
+                    {
+                        var LNMD = streamReader.ReadLine().Split(' ').Select(long.Parse).ToArray();
+                        var L = LNMD[0];
+                        var N = LNMD[1];
+                        var M = LNMD[2];
+                        var D = LNMD[3];
+                        var W = streamReader.ReadLine().Split(' ').Select(long.Parse).ToArray();
+                        var solution = ComputeSolution(L, N, M, D, W);
+                        var output = string.Format("Case #{0}: {1}", t, solution);
+                        Console.WriteLine(output);
+                        streamWriter.WriteLine(output);
+
+                    }
+                    streamWriter.Close();
+                }
+                streamReader.Close();
+            }
+
+        }
+
+        static long ComputeSolution(long L, long N, long M, long D, long[] W)
+        {
+            if (N != W.Length) return -1; // invalid
+
+            Array.Sort(W);
+            var w_sum = W.Sum();
+            var Q = W.Select(w => L - ((L * w)/w_sum)).ToArray();
+
+            Console.WriteLine(Q);
+
+
+            return 0;
+        }
+
+    }
+}
